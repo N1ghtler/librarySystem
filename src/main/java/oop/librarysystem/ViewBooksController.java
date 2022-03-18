b231b718daad7e2a4beee73b942bea0e61905675
N1ghtler <beevadhana@gmail.com>
2022-03-18 15:49:29 +0700

    SomePart of view book

--- src/main/java/oop/librarysystem/ViewBooksController.java
+++ src/main/java/oop/librarysystem/ViewBooksController.java
@@ -4,7 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
-public class ViewBooks {
+public class ViewBooksController {
     @FXML
     public TableView books;
     public TableColumn bookName;
