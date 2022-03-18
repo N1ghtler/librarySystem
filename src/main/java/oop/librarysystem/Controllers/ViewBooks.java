3794ab8df36d8a50a772b44d2760ce7fb09523c7
N1ghtler <beevadhana@gmail.com>
2022-03-18 15:38:20 +0700

    SomePart of view book

--- src/main/java/oop/librarysystem/Controllers/ViewBooks.java
+++ src/main/java/oop/librarysystem/Controllers/ViewBooks.java
@@ -1,4 +1,14 @@
 package oop.librarysystem.Controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class ViewBooks {
+    @FXML
+    public TableView books;
+    public TableColumn bookName;
+    public TableColumn bookType;
+    public TableColumn bookAuthor;
+    public TableColumn bookYear;
 }
