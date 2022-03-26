5cc143ffed3687627bd5c84c25e1bccf798130d5
N1ghtler <beevadhana@gmail.com>
2022-03-26 16:18:50 +0700

    FIX

--- src/main/java/oop/librarysystem/ViewBookApplication.java
+++ src/main/java/oop/librarysystem/ViewBookApplication.java
@@ -9,7 +9,7 @@ public class ViewBookApplication extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("ViewBook.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello! Team Apple");
         stage.setScene(scene);
         stage.show();
