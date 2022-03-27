ac84edc3e48bf366ac83c3c5606526f0853791fb
kanhchna <kanhna.roth@student.cadt.edu.kh>
2022-03-27 12:01:27 +0700

    can run ...

--- src/main/java/oop/librarysystem/HelloApplication.java
+++ src/main/java/oop/librarysystem/HelloApplication.java
@@ -10,8 +10,8 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("addmember.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 140);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Add-member.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello! Team Apple");
         stage.setScene(scene);
         stage.show();
