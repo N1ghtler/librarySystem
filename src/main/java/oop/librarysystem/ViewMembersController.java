9dcf3ee0ec69213400d34f1103506c3b7a88143a
N1ghtler <beevadhana@gmail.com>
2022-04-12 09:07:34 +0700

    DOne

--- src/main/java/oop/librarysystem/ViewMembersController.java
+++ src/main/java/oop/librarysystem/ViewMembersController.java
@@ -6,7 +6,6 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
@@ -16,14 +15,11 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Objects;
 import java.util.ResourceBundle;
+
 
 public class ViewMembersController implements Initializable {
 
-    private Stage stage;
-    private Scene scene;
-    private Parent root;
 
 
     @FXML
@@ -40,6 +36,7 @@ public class ViewMembersController implements Initializable {
     public TextField InputIDMember;
     public Button DeleteButton;
     public Label OUTresulfLable;
+    public Parent mainFX;
 
 
     ObservableList<ViewMember> data = FXCollections.observableArrayList();
@@ -69,18 +66,42 @@ public class ViewMembersController implements Initializable {
 
 
     }
-
+    //SET SCENE
     @FXML
     public void viewBook(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("ViewBook.fxml")));
-        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+        Parent root = FXMLLoader.load(getClass().getResource("ViewBook.fxml"));
+        Stage window = (Stage) mainFX.getScene().getWindow();
+        window.setScene(new Scene(root));
     }
 
+    @FXML
+    public void addBook(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource(""));
+        Stage window = (Stage) mainFX.getScene().getWindow();
+        window.setScene(new Scene(root));
+    }
 
+    @FXML
+    public void addMember(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Add-member.fxml"));
+        Stage window = (Stage) mainFX.getScene().getWindow();
+        window.setScene(new Scene(root));
+    }
 
+    @FXML
+    public void viewBorrowBook(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("BorrowBooks.fxml"));
+        Stage window = (Stage) mainFX.getScene().getWindow();
+        window.setScene(new Scene(root));
+    }
+
+    @FXML
+    public void addBorrowBook(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("addBorrowBooks.fxml"));
+        Stage window = (Stage) mainFX.getScene().getWindow();
+        window.setScene(new Scene(root));
+    }
+    //END SET SCENE
     @FXML
     public void deleteData(ActionEvent event){
         String ID = InputIDMember.getText();
@@ -97,6 +118,14 @@ public class ViewMembersController implements Initializable {
         if (foundIT){
             System.out.println("Deleting");
             OUTresulfLable.setText("Deleting");
+            try {
+                wait(2000);
+                OUTresulfLable.setText("Done");
+                wait(5000);
+                OUTresulfLable.setText("");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
         else {
             System.out.println("NOT FOUND!");
